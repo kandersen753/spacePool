30fc3f6bcad9d690d2a03e55591f803ed7340739
Kurtis T Andersen <kandersen@unr.edu>
2016-05-31 15:23:45 -0700

    new update

---
+++ Assets/scripts/rollBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rollBall : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveZ = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveZ, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/scripts/spinMode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinMode : MonoBehaviour {
+
+    public float spinSpeed;
+    private Vector3 spinner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spinner = new Vector3(0.0f, spinSpeed, 0.0f);
+        transform.Rotate(-spinner * Time.deltaTime);
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag ("killPlanet"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
